c6fceeb8e5a48a48a03e70b249b0eab520fcbcf2
Darc Inc <darcinc@protonmail.com>
2017-09-02 08:54:50 -0400

    Updating zipfile example

--- archives/zipfiles/main.go
+++ archives/zipfiles/main.go
@@ -1,17 +1,81 @@
+/*
+BSD 2-Clause License
+
+Copyright (c) 2017, Darc Inc
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 package main
 
 import (
 	"archive/zip"
-	"bytes"
+	"fmt"
+	"io"
+	"os"
 )
 
-func main() {
-	buffer := new(bytes.Buffer)
-	zipWriter := zip.NewWriter(buffer)
+func createZip() {
+	zipFile, _ := os.OpenFile("lorem.zip", os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0644)
+	defer zipFile.Close()
+
+	zipWriter := zip.NewWriter(zipFile)
 	defer zipWriter.Close()
 
-	dataWriter, _ := zipWriter.Create("myfile.txt")
+	inputFile, _ := os.Open("lorem.txt")
+	defer inputFile.Close()
 
-	dataWriter.Write([]byte("The end of the world as we know it."))
+	dataWriter, _ := zipWriter.Create("lorem.txt")
+	io.Copy(dataWriter, inputFile)
 
+	zipWriter.Flush()
+}
+
+func unpackZip() {
+	zipFile, _ := os.Open("lorem.zip")
+	defer zipFile.Close()
+
+	fileinfo, _ := os.Stat("lorem.zip")
+	zipReader, _ := zip.NewReader(zipFile, fileinfo.Size())
+
+	for _, z := range zipReader.File {
+		file, _ := z.Open()
+
+		io.Copy(os.Stdout, file)
+	}
+}
+
+func fileSizes() {
+	origInfo, _ := os.Stat("lorem.txt")
+	zipInfo, _ := os.Stat("lorem.zip")
+
+	fmt.Println()
+	fmt.Println()
+	fmt.Printf("Original %d bytes but zipped %d bytes\n", origInfo.Size(), zipInfo.Size())
+}
+
+func main() {
+	createZip()
+	unpackZip()
+	fileSizes()
 }
